9dff01fbdbd58f55aee1f7e8b83ee1d111374d47
rimi2807 <rimitariq12807@gmail.com>
2024-06-28 20:04:03 +0500

    env not working so adding directly to db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = process.env.VITE_DATABASE;
+var mongoURL = "mongodb+srv://tramsha4:Ramsha-5t5r@cluster0.aysrqcc.mongodb.net/mern-pizza-delivery";
 
 mongoose.connect(mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
 
